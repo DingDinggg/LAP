2d843a3ad147723ab88726a7971212f63e5bb55a
DingDinggg <122292034+DingDinggg@users.noreply.github.com>
2024-03-22 11:38:42 +0700

    Lap1,2,3,4,5,6

---
+++ Lap4_Lap5_Lap6_NguyenPhanXuanVinh/footer.php
@@ -0,0 +1,13 @@
+</div>
+
+</div>
+</div>
+<div id="wrapper" class="container">
+    <footer>
+        Quay lại <a href="/demo/lap3">Trang chủ</a><br />
+        &#169; 2022 - Trendemy
+    </footer>
+</div>
+</body>
+
+</html>
---
+++ Lap4_Lap5_Lap6_NguyenPhanXuanVinh/index.php
@@ -0,0 +1,18 @@
+<?php
+include_once("header.php");
+?>
+<!-- lab 4 là 4,5,6 -->
+<ul>
+    <li>
+        <a href="product-list.php">Danh sách sản phẩm</a>
+    </li>
+
+    <li>
+        <a href="product-add.php">Thêm sản phẩm</a>
+    </li>
+
+</ul>
+
+<?php
+include_once("footer.php");
+?>
